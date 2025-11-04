f9e93658355cfff66bb2f2a6a3e8fe260fdac30f
suveksha911 <suve345@gmail.com>
2025-11-04 12:15:36 +0545

    change

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const double PI = 3.14;
+
+double radius = 3;
+double area = PI * radius * radius;
+double perimeter = 2 * PI * radius;
+
+Console.WriteLine($"Area of circle: {area}");
+Console.WriteLine($"Perimeter of circle: {perimeter}");
